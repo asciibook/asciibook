6348e000d880b595739d1c58dc4b53dd93e61e7d
Rei <chloerei@gmail.com>
2019-12-25 23:01:19 +0800

    Add simple theme layout

--- lib/asciibook/book.rb
+++ lib/asciibook/book.rb
@@ -9,29 +9,42 @@ module Asciibook
       asciidoc = Asciidoctor.load_file @source, backend: 'htmlbook'
       doc = REXML::Document.new asciidoc.convert(header_footer: true)
 
-      output = ""
-      doc.write output: output, indent: 2
-
       path = File.dirname(@source)
       build_path = File.join(path, 'build/html')
       FileUtils.rm_r build_path
       FileUtils.mkdir_p build_path
 
       book_title = doc.elements['html/head/title'].text
-      layout = Liquid::Template.parse(File.read(File.expand_path('../../../themes/default/html/layout.html', __FILE__)))
+      theme_path = File.expand_path('../../../themes/default/', __FILE__)
+      layout = Liquid::Template.parse(File.read(File.join(theme_path, 'html', 'layout.html')))
       doc.elements.each('html/body/section') do |section|
-        File.open(File.join(build_path, section['id']), 'w') do |file|
+        File.open(File.join(build_path, "#{section['id']}.html"), 'w') do |file|
           file.write layout.render({
             'book' => {
-              'title' => book_title
+              'title' => book_title,
+              'toc' => book_toc(doc)
             },
             'page' => {
-              'title' => section.elements['h1'].text,
-              'content' => doc.elements['html/body'].elements.map(&:to_s).join
+              'title' => section.elements['h1']&.text,
+              'content' => section.to_s
             }
           })
         end
       end
+
+      # copy theme assets
+      theme_assets = Dir.glob(File.join(theme_path, 'html', '*'))
+      FileUtils.cp_r theme_assets, build_path
+    end
+
+    # Fixme: url from anchor to page url
+    # Todo: tree toc
+    def book_toc(doc)
+      toc = []
+      doc.elements.each('html/body/nav/ol/li/a') do |element|
+        toc << { 'title' => element.text, 'url' => element['href'] }
+      end
+      toc
     end
   end
 end
---
+++ themes/default/html/default.js
@@ -0,0 +1,5 @@
+(function() {
+  document.getElementById('toc-button').addEventListener('click', function() {
+    document.getElementById('toc').classList.toggle('active');
+  })
+})();
