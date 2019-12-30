18639f7d050b823089d06f52566b82d51153410d
Rei <chloerei@gmail.com>
2019-12-30 17:38:14 +0800

    Edit html layout
    - remove second level toc for simple style
    - add mobile toc drawer style

--- themes/default/html/default.js
+++ themes/default/html/default.js
@@ -1,5 +1,36 @@
 (function() {
-  document.getElementById('toc-button').addEventListener('click', function() {
-    document.getElementById('toc').classList.toggle('active');
+  var drawerEnabled = localStorage.getItem('drawerEnabled');
+  var drawer = document.getElementById('drawer')
+
+  var isDesktop = function() {
+    return window.innerWidth > 1040;
+  };
+
+  var initDrawer = function() {
+    if (isDesktop() && drawerEnabled != 'false') {
+      drawer.classList.add('active');
+    }
+  }
+
+  initDrawer();
+  window.addEventListener('resize', initDrawer);
+
+  document.getElementById('drawer-button').addEventListener('click', function() {
+    drawer.classList.toggle('active');
+
+    if (isDesktop()) {
+      localStorage.setItem('drawerEnabled', drawer.classList.contains('active'));
+    }
+  })
+
+  document.getElementById('drawer-backdrop').addEventListener('click', function() {
+    drawer.classList.toggle('active');
+  });
+
+  document.querySelectorAll('#drawer a').forEach(function(element) {
+    var url = new URL(element.href, document.baseURI).href;
+    if (url == document.URL) {
+      element.classList.add('active');
+    }
   })
 })();
