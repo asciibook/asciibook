436fede3f9987f7902f7447d209d25f7529a5e21
Rei <chloerei@gmail.com>
2020-03-02 20:19:41 +0800

    Add twtter/og tags for html theme

--- lib/asciibook/page.rb
+++ lib/asciibook/page.rb
@@ -14,7 +14,19 @@ module Asciibook
     end
 
     def content
-      node.convert
+      @content ||= node.convert
+    end
+
+    def doc
+      @doc ||= Nokogiri::HTML.fragment(content)
+    end
+
+    def image_url
+      doc.css('img').first&.attr('src')
+    end
+
+    def description
+      doc.css('p').first&.text
     end
 
     def to_hash
@@ -22,6 +34,8 @@ module Asciibook
         'path' => path,
         'title' => title,
         'content' => content,
+        'image_url' => image_url,
+        'description' => description,
         'prev_page' => prev_page && { 'path' => prev_page.path, 'title' => prev_page.title },
         'next_page' => next_page && { 'path' => next_page.path, 'title' => next_page.title }
       }
--- test/asciibook/page_test.rb
+++ test/asciibook/page_test.rb
@@ -1,4 +1,21 @@
 require 'test_helper'
 
 class Asciibook::PageTest < Asciibook::Test
+  def test_image_path
+    doc = Asciidoctor.load <<~EOF
+      image::image/logo.png[]
+    EOF
+    page = Asciibook::Page.new path: 'test.html', node: doc
+    assert_equal 'image/logo.png', page.image_url
+  end
+
+  def test_description
+    doc = Asciidoctor.load <<~EOF
+      text description
+
+      other text
+    EOF
+    page = Asciibook::Page.new path: 'test.html', node: doc
+    assert_equal 'text description', page.description
+  end
 end
